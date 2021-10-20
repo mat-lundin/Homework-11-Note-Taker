59f30563d018a8449f7859975d217ec1cd934901
Mat Lundin <matmll12@gmail.com>
2021-10-20 16:14:58 -0500

    added get and post functions for notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,13 @@
 const notes = require('express').Router();
 
+//logic for getting the notes and returning that as the response
+notes.get('/',(req, res) => {
 
+})
 
+//logic for appending new note and updating the db 
+notes.post('/', (req,res) => {
 
+})
 
 module.exports = notes;
