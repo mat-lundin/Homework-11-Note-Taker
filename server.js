c1acb8fa7fb41dd65a296c13f5da7e762c6a7a27
Mat Lundin <matmll12@gmail.com>
2021-10-21 16:52:26 -0500

    fixed port const

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const htmlRouter = require('./routes/htmlroutes.js');
 const apiRouter = require('./routes/apiroutes')
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 // const PORT = 3001;
 
 const app = express();
